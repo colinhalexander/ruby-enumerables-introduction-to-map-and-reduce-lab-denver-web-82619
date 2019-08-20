59f42866291884c77567e5812babec13e2628f54
Colin Alexander <chalexander@me.com>
2019-08-20 16:23:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,11 +34,11 @@ def map_to_square(source_array)
   return result
 end
 
-def reduce_to_total(source_array)
+def reduce_to_total(source_array, starting_point = 0)
   count = 0
-  result = 0
+  result = starting_point
   while source_array[count] do
-    result[count] += source_array[count]
+    result += source_array[count]
     count += 1
   end
   return result
