76d6aba946db4519a73c6508e121ad221f02dbe2
Colin Alexander <chalexander@me.com>
2019-08-20 16:25:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,13 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  
+  count = 0
+  bool = true
+  while source_array[count] do
+    bool = bool && source_array[count]
+    count += 1
+  end
+  return bool
 end
 
 def reduce_to_any_true(source_array)
